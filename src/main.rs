748c40e3d0a2dbc57c3f23037b9515806a7c2eee
edo1z <endo.yuta@logicky.com>
2023-12-02 12:46:50 +0900

    log出力してみた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use tracing::{error, info, instrument};
+
+#[instrument]
+async fn process_data(data: &String) -> Result<(), Box<dyn Error>> {
+    info!("処理中のデータ: {}", data);
+    Ok(())
+}
+
+#[instrument]
+async fn process_error(data: &String) -> Result<(), Box<dyn Error>> {
+    info!("処理中のデータ: {}", data);
+    Err("エラー!".into())
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let data = "サンプルデータ".to_string();
+
+    let _ = process_data(&data).await;
+
+    if let Err(e) = process_error(&data).await {
+        error!("エラー: {}", e);
+    }
+
+    info!("hoge");
 }
