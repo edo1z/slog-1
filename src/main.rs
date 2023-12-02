31e05565554631302eb8aa14fa106afbec51cdd6
edo1z <endo.yuta@logicky.com>
2023-12-02 14:24:51 +0900

    ファイルパス・行番号を表示してみた

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,89 @@
-use std::error::Error;
-use tracing::{error, info, instrument};
+use std::{error::Error, fmt};
+use tracing::{error, instrument};
 
-#[instrument]
-async fn process_data(data: &String) -> Result<(), Box<dyn Error>> {
-    info!("処理中のデータ: {}", data);
-    Ok(())
+// カスタムエラータイプ
+#[derive(Debug)]
+struct MyRepoError {
+    message: String,
+    file: &'static str,
+    line: u32,
 }
 
+impl MyRepoError {
+    fn new(message: String, file: &'static str, line: u32) -> MyRepoError {
+        MyRepoError {
+            message,
+            file,
+            line,
+        }
+    }
+}
+
+impl fmt::Display for MyRepoError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} ({}:{})", self.message, self.file, self.line)
+    }
+}
+
+impl Error for MyRepoError {}
+
+// MyServiceErrorは、内部エラー（MyRepoError）をラップする
+#[derive(Debug)]
+struct MyServiceError {
+    source: MyRepoError,
+    file: &'static str,
+    line: u32,
+}
+
+impl MyServiceError {
+    fn new(source: MyRepoError, file: &'static str, line: u32) -> MyServiceError {
+        MyServiceError { source, file, line }
+    }
+}
+
+impl fmt::Display for MyServiceError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} <- ({}:{})", self.source, self.file, self.line)
+    }
+}
+
+impl Error for MyServiceError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(&self.source)
+    }
+}
+
+// 非同期のリポジトリ関数
 #[instrument]
-async fn process_error(data: &String) -> Result<(), Box<dyn Error>> {
-    info!("処理中のデータ: {}", data);
-    Err("エラー!".into())
+async fn repository(a: i32, b: i32) -> Result<(), MyRepoError> {
+    Err(MyRepoError::new(
+        "repository ERROR".to_string(),
+        file!(),
+        line!(),
+    ))
+}
+
+// 非同期のサービス関数
+#[instrument]
+async fn service(a: i32, b: i32) -> Result<(), MyServiceError> {
+    repository(a, b)
+        .await
+        .map_err(|e| MyServiceError::new(e, file!(), line!()))
+}
+
+// 非同期のユースケース関数
+#[instrument]
+async fn use_case(x: i32, y: i32) {
+    if let Err(e) = service(x * 2, y * 2).await {
+        error!("use_case ERROR: {}", e);
+    }
 }
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let data = "サンプルデータ".to_string();
-
-    let _ = process_data(&data).await;
-
-    if let Err(e) = process_error(&data).await {
-        error!("エラー: {}", e);
-    }
-
-    info!("hoge");
+    let x = 1;
+    let y = 2;
+    use_case(x, y).await;
 }
